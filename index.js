2914d8640f9c9f15b51a6db02e79562f2661cd21
nizamudheenknr <nizamudheenknr@gmail.com>
2024-05-03 10:48:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// const fs = require("fs")
+
+//  fs.writeFile("text.txt","hello world jghh",(error)=>{
+//     if(error) {
+//         console.log("error");
+//     }
+//     console.log("succuss");
+//  })
+
+//  fs.readFile("text.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data);
+//  })
+//  console.log('hello');
+ 
+//  fs.unlink("text.txt",()=>{
+//     if(err){
+//         console.log("err");
+//     }
+
+//     console.log("success");
+//  })
+
+//................ EXPRERSS.JS...........//
+// .......................................//
+// ...........................................//
+// .......<<<<<<<<<<>>>>>>>>>>>>>>><<<<<>>>>//
+// >>>>>>>>>><<<<<<<<<<<<>>>>>>>>>>><<<<<<<<<<<<<<<//
+
+ 
+const {logger,myName,my, Name} = require("./utils")
+
+
+console.log(logger());
+console.log(myName);
+console.log(Name);
+console.log(my());
+
+ 
---
+++ utils.js
@@ -0,0 +1,18 @@
+const logger = ()=>{
+    return 2 * 5
+}
+const Name = "hello"
+
+const my = ()=>{
+    return 3 * 5
+}
+const myName = "Hello world"
+
+module.exports = {
+    logger,
+    myName,
+    my,
+    Name
+}
+
+
